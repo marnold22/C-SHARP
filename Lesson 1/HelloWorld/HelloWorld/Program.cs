abb67ce3c082e7c146ff4a535bf3f5c7bc1cc3d8
Michael <minimearnold@gmail.com>
2020-05-19 08:51:28 -0700

    Lesson 3 - Declaring Variables

--- Lesson 1/HelloWorld/HelloWorld/Program.cs
+++ Lesson 1/HelloWorld/HelloWorld/Program.cs
@@ -6,13 +6,24 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // Lesson 1
+            // Lesson 1 - Hello World
             Console.WriteLine("Hello World!");
+            Console.Write("\n");
 
-            // Lesson 2
+
+            // Lesson 2 - WriteLine() vs Write()
             Console.WriteLine("WriteLine() will print on a new line each time.");
-            Console.Write("Write() will print all on the same line.");
-            
+            Console.Write("Write() will print all on the same line. \n");
+            Console.Write("\n");
+
+
+            // Lesson 3 - Declaring Variables
+            string name = "Michael";
+            int age = 25;
+            double height = 5.7D;
+            bool male = true;
+            Console.WriteLine("Hello my name is " + name + " and I am " + age + " years old");
+
         }
     }
 }
