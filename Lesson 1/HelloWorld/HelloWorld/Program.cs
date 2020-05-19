f7c8369bc18330b55f4280f16de9169c9d0b2bd0
Michael <minimearnold@gmail.com>
2020-05-19 08:37:01 -0700

    Lesson 2 Write() vs WriteLine()

--- Lesson 1/HelloWorld/HelloWorld/Program.cs
+++ Lesson 1/HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            // Lesson 1
             Console.WriteLine("Hello World!");
+
+            // Lesson 2
+            Console.WriteLine("WriteLine() will print on a new line each time.");
+            Console.Write("Write() will print all on the same line.");
+            
         }
     }
 }
